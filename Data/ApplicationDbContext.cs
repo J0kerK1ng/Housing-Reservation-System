fa142ddf910cea633c3d378ff528bca86392c151
J0kerK1ng <85815360+J0kerK1ng@users.noreply.github.com>
2023-02-01 18:57:28 -0500

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Housing_Reservation_System.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Housing_Reservation_System.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options) 
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Housing_Reservation_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
